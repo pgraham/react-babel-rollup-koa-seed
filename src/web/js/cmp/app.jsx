20cc00a90eabea92f72e960338f36a6bc7dff2cf
Philip Graham <philip.robert.graham@gmail.com>
2019-07-01 12:04:07 +0000

    Add a non-connected component to the base app

--- src/web/js/cmp/app.jsx
+++ src/web/js/cmp/app.jsx
@@ -6,8 +6,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import { Post } from './post'
+
 function App (props) {
-  return <ul>{props.posts.map((post, idx) => <li key={idx}>{post}</li>)}</ul>
+  return (
+    <ul>
+      {props.posts.map((post, idx) => (
+        <li key={idx}>
+          <Post content={post} />
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default connect(state => ({ posts: state.posts }))(App)
---
+++ src/web/js/cmp/post.jsx
@@ -0,0 +1,11 @@
+'use strict'
+
+/**
+ * Post component
+ */
+
+import React from 'react'
+
+export function Post ({ content }) {
+  return <div className='post'>{content}</div>
+}
